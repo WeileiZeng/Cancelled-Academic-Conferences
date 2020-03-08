afef72928adc188f3cac126ad2b053104c62a384
Weilei Zeng <240155787@qq.com>
2020-03-08 13:39:47 -0700

    update content; add github action

--- download.sh
+++ download.sh
@@ -1,1 +1,5 @@
+# download file from google sheet
 curl "https://docs.google.com/spreadsheets/d/1O3wnaFYSZCgY3Ih4yDw3EIH2SC_-vjhyHwrCQSy0J7M/export?gid=0&format=csv" -o conferences.csv
+
+# convert csv to yaml
+python3 gsheet2yaml.py
